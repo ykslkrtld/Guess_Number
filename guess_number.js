0b4f9b4ecf539e1dfc8d836a975642c2401572c3
yksl <yukselkurtuldu9@gmail.com>
2024-02-24 14:26:02 +0300

    editted

--- guess_number.js
+++ guess_number.js
@@ -1,8 +1,60 @@
-// const number = Math.round(Math.random() * 100);
-// let right = 10;
-// let low = 0;
-// let high = 100;
-// console.log(number);
+const input = document.getElementById("tahmin")
+const kontrol = document.getElementById("kontrol")
+const sonuc = document.getElementById("sonuc")
+const tekrar = document.getElementById("tekrar")
+let right = 5
+let low = 0
+let high = 100
+let number = Math.round(Math.random() * 100);
+console.log(number);
+
+window.onload = () =>{
+    input.focus()
+}
+
+kontrol.addEventListener("click", () =>{
+    let guess = input.value
+    input.focus()
+    if (guess > 100 || guess < 0) {
+        sonuc.textContent = 'Lütfen 0-100 arasında geçerli bir sayı giriniz!';
+        input.value = ''
+        input.focus()
+        return
+    }
+    if(right > 0){
+        right--
+        if (guess == number){
+            sonuc.textContent =`Tebrikler doğru tahmin, sayi : ${number}`
+            document.querySelector(".container").style.backgroundColor = "green"
+            return
+        } else if(right == 0){
+            sonuc.textContent = `Oyun bitti maalesef başka hakkınız kalmadı doğru sayi ${number} idi`
+            document.querySelector(".container").style.backgroundColor = "red"
+            input.focus()
+            input.value = ""
+            return
+        } else if(guess > number){
+            high = guess
+            input.focus()
+            input.value = ""
+            sonuc.textContent =`Yanlış tahmin. ${right} hakkınız kaldı. Lütfen ${low} ile ${high} arasında bir sayı tahmin ediniz:`;
+        } else if(guess < number){
+            low = guess
+            input.focus()
+            input.value = ""
+            sonuc.textContent =`Yanlış tahmin. ${right} hakkınız kaldı. Lütfen ${low} ile ${high} arasında bir sayı tahmin ediniz:`;
+        } 
+    }
+}
+)
+
+
+
+
+
+
+
+
 
 
 // guess = Number(prompt("Lütfen 0-100 arasında bir sayı tahmin ediniz:"));
